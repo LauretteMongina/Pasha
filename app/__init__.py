1b7713985a0c8e164c7b26b0fa452148bf5ef4df
laurettemongina <monginalaurette2gmail.com>
2021-09-13 11:33:17 +0300

    Adds an init.py.

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from config import config_options
+
+# bootstrap = Bootstrap()
+
+def create_app(config_name):
+    app = Flask(__name__,instance_relative_config = True,static_url_path='/static')
+    app.config.from_object(config_options[config_name])
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .requests import configure_request
+    configure_request(app)
+
+    return app
