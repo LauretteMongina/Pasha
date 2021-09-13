11c1805404b1ee8618229e6406552ae298f47280
laurettemongina <monginalaurette2gmail.com>
2021-09-13 11:34:59 +0300

    Adds requests to get articles by category and get articles from source.

--- app/requests.py
+++ app/requests.py
@@ -91,3 +91,64 @@ def process_new_articles(articles_list):
 
 # get news relating to specific source and country
 
+def get_articles_from_source(source):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_articles_url = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'.format(source,
+                                                                                                      api_key)
+    # get_articles_url = base_url_articles.format(source_id, api_key)
+    with urllib.request.urlopen(get_articles_url) as url:
+        get_articles_data = url.read()
+        # load the data into a json object
+        get_articles_response = json.loads(get_articles_data)
+
+        articles_results = None
+
+        if get_articles_response['articles']:
+            articles_results_list = get_articles_response['articles']
+            articles_results = process_results_articles(articles_results_list)
+    return articles_results  # return the results
+
+# get news depending on category
+
+def get_articles_depending_on_category(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_articles_url = 'https://newsapi.org/v2/top-headlines?category={}&apiKey={}'.format(
+        category,api_key)
+    # get_articles_url = base_url_articles.format(source_id, api_key)
+    with urllib.request.urlopen(get_articles_url) as url:
+        get_articles_data = url.read()
+        # load the data into a json object
+        get_articles_response = json.loads(get_articles_data)
+
+        articles_results = None
+
+        if get_articles_response['articles']:
+            articles_results_list = get_articles_response['articles']
+            articles_results = process_new_articles(articles_results_list)
+    return articles_results  # return the results
+
+
+def process_category_results(articles_list):
+    '''
+    function that processes the json files of articles from the api key
+    '''
+    articles_results = []
+    for article_item in articles_list:
+       
+        author = article_item.get('author')
+        description = article_item.get('description')
+        url = article_item.get('url')
+        urlToImage = article_item.get('urlToImage')
+        publishedAt = article_item.get('publishedAt')
+        title = article_item.get ('title')
+        content= article_item.get('content')
+        if url:
+            new_articles = Articles(
+            author, title, description, url, urlToImage, publishedAt,content)
+            articles_results.append(new_articles)
+
+    return articles_results
