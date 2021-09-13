b49040ace663bac734b740d79b82dd0b552b512f
laurettemongina <monginalaurette2gmail.com>
2021-09-13 11:39:17 +0300

    Creates a health, entertainment and sports view functions.

--- app/main/views.py
+++ app/main/views.py
@@ -24,3 +24,37 @@ def news_articles(id):
     articles=get_articles(id)
     
     return render_template('articles.html',articles=articles)
+@main.route('/entertainment')
+def entertainment():
+    '''
+    View entertainment page function that returns the entertainment page and its data
+    '''
+    entertainment = get_articles_depending_on_category('entertainment')
+    title = 'Entertainment - Welcome to The best Hot News in the world'
+    return render_template('entertainment.html',
+                           title=title,
+                           entertainment = entertainment
+                           )
+@main.route('/sports')
+def sports():
+    '''
+    View sports page function that returns the sports page and its data
+    '''
+    sports = get_articles_depending_on_category('sports')
+    title = 'Sports - Welcome to The best Hot News in the world'
+    return render_template('sports.html',
+                           title=title,
+                           sports = sports
+                           )
+
+@main.route('/health')
+def health():
+    '''
+    View health page function that returns the health page and its data
+    '''
+    health = get_articles_depending_on_category('health')
+    title = 'Health - Welcome to The best Hot News in the world'
+    return render_template('health.html',
+                           title=title,
+                           health = health
+                           )
